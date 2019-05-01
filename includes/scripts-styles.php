136c6f81300eea630870e6d2c510ad6cddb9f322
Ash Whiting <ash_whiting@hotmail.com>
2019-05-01 12:35:33 +0100

    updated structure

--- includes/scripts-styles.php
+++ includes/scripts-styles.php
@@ -15,23 +15,20 @@ function seed_scripts_and_styles() {
 		$assetstr = file_get_contents(dirname(dirname(__FILE__))."/build/manifest.json");
 		$assets = json_decode($assetstr, true);
 		$assets     = array(
-			'css' => '/build/css/styles.min.css' . '?' . $assets['build/css/styles.min.css']['hash'],
-			'js'  => '/build/js/scripts.min.js' . '?' . $assets['build/js/scripts.min.js']['hash'],
+			'css' => '/build/css/styles.css' . '?' . $assets['build/css/styles.css']['hash'],
+			'vendor-scripts' => '/build/js/vendor.js' . '?' . $assets['build/js/vendor.js']['hash'],
+			'js'  => '/build/js/app.js' . '?' . $assets['build/js/app.js']['hash'],
 		);
 
-		wp_register_script( 'modernizr', get_stylesheet_directory_uri() . '/build/js/modernizr.min.js' );
-
-		wp_register_style( 'seed-stylesheet', get_stylesheet_directory_uri() . $assets['css'], array(), '', 'all' );
-		wp_enqueue_style( 'seed-stylesheet' );
+		wp_register_style( 'styles', get_stylesheet_directory_uri() . $assets['css'], array(), '', 'all' );
 
 		wp_deregister_script( 'jquery' );
-		wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', false, '1.11.3', true);
 		wp_register_script( 'scripts', get_stylesheet_directory_uri() . $assets['js'], array(), '', true );
 
 		// Do it.
-		wp_enqueue_script( 'modernizr' );
-		wp_enqueue_script( 'jquery' );
+		wp_enqueue_style( 'styles' );
 		wp_enqueue_script( 'scripts' );
+		wp_enqueue_script( 'vendor' );
 
 	}
 }
